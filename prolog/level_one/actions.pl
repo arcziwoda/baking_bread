506c63ef38ae214298cab475a5ce65dc2944990a
akempins <01178535@pw.edu.pl>
2024-10-15 13:09:32 +0200

    now player can finish level one only after talking to saul

--- prolog/level_one/actions.pl
+++ prolog/level_one/actions.pl
@@ -4,14 +4,17 @@
 
 lookAround :-
     current_level(1),
+
     decrement_time_left,
     write("Walter: Let's see what's on the table..."), nl,
-    write("[sees sugar, blue food coloring, baking soda and los pollos hermanos sauce]"), nl.
+    write("[sees sugar, blue food coloring, baking soda and los pollos hermanos sauce]"), nl,
+    !.
 
 % The user adds correct ingredients to the muffins
 add(muffins, Ingredient) :-
     current_level(1),
     muffins_done,
+
     decrement_time_left,
     write("Jesse: Yo, the muffins are done, b****! Anwser that phone!"), nl,
     !.
@@ -19,6 +22,7 @@ add(muffins, Ingredient) :-
 add(muffins, Ingredient) :-
     current_level(1),
     correct_ingredient(Ingredient),
+
     write("Walter: That's it! The final ingredient for the muffins is "), write(Ingredient), write("!"), nl,
     write("Jesse: Yo, Mr. Black! These muffins are gonna be tight!"), nl,
     write("Walter: Yes, Jesse. Now let's finish these muffins and move on."), nl,
@@ -34,11 +38,11 @@ add(muffins, Ingredient) :-
 add(muffins, Ingredient) :-
     current_level(1),
     explosive_ingredient(Ingredient),
+
     write("Walter: Damn it! The oven overheated! The alarms are going off!"), nl,
     write("Jesse: The police will come soon, they will know about the secret bakery!"), nl,
     write("[The phone rings]"), nl,
     write("Jesse: Yo, Mr. Black! It's Saul! Better pick that up!"), nl,
-    write("<Saul Badman>: Hey, genius! What did you do? The cops are on their way! You better run!"), nl,
     initialize_time_left(20),
     assert(police_is_coming),
     assert(muffins_done),
@@ -49,6 +53,7 @@ add(muffins, Ingredient) :-
     current_level(1),
     \+ correct_ingredient(Ingredient),
     \+ explosive_ingredient(Ingredient),
+
     decrement_time_left,
     write("Walter: No, that's not right. We need to add the correct ingredient to the muffins."), nl,
     write("Jesse: Come on, think! What was it again?"), nl,
@@ -57,6 +62,7 @@ add(muffins, Ingredient) :-
 % The user adds ingredients to the wrong object
 add(_, _) :-
     current_level(1),
+
     decrement_time_left,
     write("Walter: We are not adding ingredients to that! Focus on the muffins!"), nl,
     !.
