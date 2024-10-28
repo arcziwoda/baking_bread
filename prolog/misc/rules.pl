d439b53677a8af455baebb046af374c0d6fe173d
zgodek <ziuta@email.com>
2024-10-28 23:33:30 +0100

    updated look_around everywhere so it doesnt take time

--- prolog/misc/rules.pl
+++ prolog/misc/rules.pl
@@ -4,7 +4,7 @@ help :-
     write("---------------------------------------------"), nl,
     write("Yo, here are the commands you can use, b****:"), nl,
     write("1. talk_to(name?) - Yo, talk to this dude or dudette, keep the convo going, man!"), nl,
-    write("2. look_round - Yo, check out what you can see!"), nl,
+    write("2. look_around - Yo, check out what you can see!"), nl,
     write("3. add(where?, what?) - Yo, add something to something, simple, got it?"), nl,
     write("4. use(what?) - Yo, you can use something you have."), nl,
     write("5. go_to(where?) - Yo, go to a place, or somethin'."), nl,
